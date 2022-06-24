321ff4abd4aa1164d39ea28b85c1ad974649ae51
Chris Felicien <f.ilungakalenga@gmail.com>
2022-06-24 14:44:56 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const buttons = document.querySelector(".set");
+
+const handleClick = function (e) {
+  const button = e.target;
+  if (button.classList.contains("set")) return;
+  //   console.log(button);
+  const buttonValue = button.textContent;
+  playSong(buttonValue);
+};
+
+const playSong = function (btn) {
+  const son = `./song/${btn}.mp3`;
+  const song = new Audio(son);
+  return song.play();
+};
+
+// click Event
+buttons.addEventListener("click", handleClick);
+
+// Key envent
+document.addEventListener("keydown", (e) => {
+  const btns = [...buttons.children];
+  btns.forEach((btn) => {
+    if (!btn.classList.contains(e.key)) return;
+    const buttonValue = e.key;
+    playSong(buttonValue);
+  });
+});
